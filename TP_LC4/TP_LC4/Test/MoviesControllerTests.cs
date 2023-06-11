26b40476dc2013dfef9760531b70db3e5baeb139
Lucianosolari <luciano3924@gmail.com>
2023-06-10 22:52:30 -0300

    Test superado con dotnet test

---
+++ TP_LC4/TP_LC4/Test/MoviesControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading.Tasks;
+using TP_LC4.Controllers;
+using TP_LC4.Models;
+using Xunit;
+
+namespace TP_LC4.Tests
+{
+    public class MoviesControllerTests
+    {
+        [Fact]
+        public async Task Create_ValidMovie_RedirectsToIndex()
+        {
+            
+            var movie = new Movie { MovieName = "Test Movie", MovieGenre = "Test Genre", MovieDuration = 120, MovieBudget = 1000000 };
+            var mockContext = new Mock<TpLc4Context>();
+            mockContext.Setup(c => c.SaveChangesAsync(default)).Returns(Task.FromResult(0));
+            var controller = new MoviesController(mockContext.Object);
+
+            
+            var result = await controller.Create(movie);
+
+            
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+    }
+}
